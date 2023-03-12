3798cb3c52bd159ffb2b983879165f4d637c47a1
Eztosin <tosinezekiel03@gmail.com>
2023-03-12 13:25:28 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 /**
 * main - program that prints its name, followed by a new line
 * @argc: argument that counts argument input
