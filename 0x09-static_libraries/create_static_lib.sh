2eed6a38f5f7d80d559f4a08c990c7c4ab2f3b1a
Eztosin <tosinezekiel03@gmail.com>
2023-03-11 21:10:34 -0800

    Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Find all .c files in the current directory
-c_files=$(find . -maxdepth 1 -name "*.c")
-# Compile each .c file into an object file
-for c_file in $c_files; do
-object_file=${c_file%.c}.o
-gcc -c $c_file -o $object_file
