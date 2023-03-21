63b5be3bcf5adabf40f0f1ce4c38c8ad344e0d81
Eztosin <tosinezekiel03@gmail.com>
2023-03-21 15:07:45 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure for a dog
+* @name: name of the dog (string)
+* @age: age of the dog (float)
+* @owner: owner of the dog (string)
+*
+* Description: defines a structure for a dog with its name, age and owner.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
