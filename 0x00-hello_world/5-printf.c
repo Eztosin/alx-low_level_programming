3266ab7f2b67befe465dae35126d61a9bdfe094f
Eztosin <tosinezekiel@gmail.com>
2023-05-27 09:42:33 +0000

    Changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 * main - print the string in the put function
 *
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+* this program prints "with proper grammar, but the outcome
+* is a piece of art"
 * Return: 0
 */
 int main(void)
