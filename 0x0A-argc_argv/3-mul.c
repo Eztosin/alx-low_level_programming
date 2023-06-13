bed66730950b690509b2c72a8b072706f1f52e6f
Eztosin <tosinezekiel@gmail.com>
2023-06-13 06:58:01 +0000

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 int j, mul = 1;
-if (argc > 1)
+if (argc > 2)
 {
 for (j = 1; j < argc; j++)
 {
@@ -18,7 +18,7 @@ mul = mul *k;
 }
 printf("%d\n", mul);
 }
-if (argc == 1)
+if (argc <= 2)
 {
 printf("Error\n");
 return (1);
