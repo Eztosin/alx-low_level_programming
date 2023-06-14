0414730de821e6405ef1d2e8f4b15c03f5023470
Eztosin <tosinezekiel@gmail.com>
2023-06-14 08:35:56 +0000

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,8 +2,9 @@
 #include <unistd.h>
 #include <string.h>
 /**
-* is_palindrome- Function that checks if a string is palindrome or not.
-* @s: Pointer variable.
+* is_palindrome - Function that checks if a string is palindrome.
+* @ch1: pointer variable.
+* @ch2: pointer variable.
 * Return: 0 if string is not palindrome.
 */
 int is_palindrome_help(char *ch1, char *ch2);
@@ -15,14 +16,15 @@ if (*s != *s1)
 {
 return (0);
 }
-return(is_palindrome_help(s, s1));
+return (is_palindrome_help(s, s1));
 }
 
 /**
-* is_palindrome_help: a helper function to be used to check if string
-* is palindrome.
-* @ch1: Array of pointer.
-* @ch2: Array of pointer.
+* is_palindrome_help- Helper function to be used to check if string
+* is palindrome or not.
+* @ch1: pointer variable.
+* @ch2: pointer variable.
+* Return: the function and check every character.
 */
 int is_palindrome_help(char *ch1, char *ch2)
 {
