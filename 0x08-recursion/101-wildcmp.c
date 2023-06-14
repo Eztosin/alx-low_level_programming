fb54b86ff572c0cb06b4678d4f1335ec319a6830
Eztosin <tosinezekiel@gmail.com>
2023-06-14 08:43:54 +0000

    Inception. Now, before you bother telling me it's impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
 #include <string.h>
 /**
- *
- *
- */
+* wildcmp - Function that compares two strings if the are identical.
+* @s1: Array of strings.
+* @s2: Array of strings containing special characters.
+* Return: 1 if identical, 0 otherwise.
+*/
 int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0' && *s2 == '\0')
@@ -16,7 +18,7 @@ return (wildcmp(s1 + 1, s2 + 1));
 }
 if (*s2 == '*')
 {
-return (wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1));
+return ((wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1)));
 }
 return (0);
 }
