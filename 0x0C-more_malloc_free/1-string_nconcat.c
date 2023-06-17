a50f9374fa24208456bf73e1f07982ea8e5d184b
Eztosin <tosinezekiel@gmail.com>
2023-06-17 12:18:29 +0000

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,31 +8,32 @@
 * string into another.
 * @s1: Initial string.
 * @s2: String to combine with s1
-^ @n: Number of characters.
+* @n: Number of characters to copy to s2.
 * Return: NULL if function fails.
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *ch;
-int w, x, y, z;
+unsigned int w, x, y, z;
+
 for (w = 0; s1[w] != '\0'; w++)
 for (x = 0; s2[x] != '\0'; x++)
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-if (n > strlen(s2))
-n = strlen(s2);
-ch = malloc(((w + x) + 1) * sizeof(char));
+if (n >= x)
+n = x;
+ch = malloc((w + n + 1) * sizeof(char));
 if (ch == NULL)
 {
 return (NULL);
 }
-for (y = 0; s1[y] != '\0'; y++)
+for (y = 0; y < w; y++)
 {
 ch[y] = s1[y];
 }
-for (z = 0; s2[z] != '\0'; z++)
+for (z = 0; z < n; z++)
 {
 ch[y + z] = s2[z];
 }
