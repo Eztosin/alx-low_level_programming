a08b3bec9dc43030c40a3a674b987a5056eb4b7e
Eztosin <tosinezekiel@gmail.com>
2023-06-19 11:50:36 +0000

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,9 +4,10 @@
 #include <string.h>
 #include <ctype.h>
 /**
-*
-*
-*
+* mul_num- Function that multiplies two positive numbers.
+* @num1: first number to be passed.
+* @num2: second number to be passed
+* Return: Error if no digit is passed.
 */
 void mul_num(char *num1, char *num2)
 {
@@ -17,7 +18,7 @@ for (j = 0; num1[j] != '\0'; j++)
 if (!isdigit(num1[j]))
 {
 printf("Error\n");
-exit (98);
+exit(98);
 }
 }
 for (k = 0; num2[k] != '\0'; k++)
@@ -25,14 +26,22 @@ for (k = 0; num2[k] != '\0'; k++)
 if (!isdigit(num2[k]))
 {
 printf("Error\n");
-exit (98);
+exit(98);
 }
 }
-x = strtoull(num1, NULL, 10);
-y = strtoull(num2, NULL, 10);
-mul = x * y;
+x = strtoul(num1, NULL, 10);
+y = strtoul(num2, NULL, 10);
+mul = x *y;
 printf("%lu\n", mul);
 }
+
+/**
+* main - Function that multiplies two positive numbers.
+* @argc: argument count.
+* @argv: argument vector.
+* Return: Always 0.
+*/
+
 int main(int argc, char *argv[])
 {
 if (argc != 3)
