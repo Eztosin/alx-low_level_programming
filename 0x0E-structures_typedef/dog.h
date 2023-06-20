a248d735864a74a2de6a7de217126cfb1f7c0b17
Eztosin <tosinezekiel@gmail.com>
2023-06-20 11:21:11 +0000

    Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
-#ifndef DOG
-#define DOG
-#define DOG_NEW
+#ifndef DOG_H
+#define DOG_H
 /**
 * struct dog - A new structure definition.
 * @name: character name.
@@ -13,7 +12,7 @@ char *name;
 float age;
 char *owner;
 };
-typedef struct dog new;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
