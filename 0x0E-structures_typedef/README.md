a24122b2b0ba10e7d4d48767cb36243906c8b749
Eztosin <tosinezekiel@gmail.com>
2023-06-20 09:09:03 +0000

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+This file contains the concept of structures and type definition in
+C programming language and exercises to enable the learning stick.
+
+-Define a new type struct dog with the following elements:
+name, type = char *
+age, type = float
+owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+#define DOG_NEW
+/**
+* struct dog - A new structure definition.
+* @name: character name.
+* @age: variable of float data type.
+* @owner: character variable.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+typedef struct dog new;
+
+#endif
