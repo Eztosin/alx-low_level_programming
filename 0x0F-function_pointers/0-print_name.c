4fccc2134aee8e847cc0890583ba0edcc2f3bb1a
Eztosin <tosinezekiel@gmail.com>
2023-06-21 21:11:57 +0000

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <unistd.h>
+#include <string.h>
+/**
+* print_name- Function that prints a name.
+* @name: char name to be printed.
+* @f: function pointer.
+* Return: Nothing.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+write(1, "\n", 0);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,6 @@
+This file contain the projects carried out in function pointers.
+Functions also have pointers which can point to the first byte of
+the code.
+
+-Write a function that prints a name.
+Prototype: void print_name(char *name, void (*f)(char *));
