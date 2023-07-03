b254506b6f413e9432667fe55032426422d2e7cd
Eztosin <tosinezekiel@gmail.com>
2023-07-03 17:37:53 +0000

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* list_len - A function that lists all elements of a list_t list.
+* @h: pointer to list head.
+* Return: Number of elements counted.
+*/
+
+size_t list_len(const list_t *h)
+{
+const list_t *ch;
+size_t count = 1;
+ch = h;
+while (ch->next != NULL)
+{
+ch = ch->next;
+count = count + 1;
+}
+return (count);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -6,4 +6,8 @@ Prototype: size_t print_list(const list_t *h);
 Return: the number of nodes
 Format: see example
 If str is NULL, print [0] (nil)
-You are allowed to use printf
+You are allowed to use printf.
+
+-A function that returns the number of elements in a linked list_t
+list.
+Prototype: size_t list_len(const list_t *h);
