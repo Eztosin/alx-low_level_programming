7221afc54846b7b35a85e614f1940c562b756b14
Eztosin <tosinezekiel@gmail.com>
2023-07-06 00:01:27 +0000

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,20 +13,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 listint_t *newptr;
 listint_t *ch;
-unsigned int i = 0;
+unsigned int i;
 newptr = malloc(sizeof(listint_t));
 if (newptr == NULL)
+{
 return (NULL);
+}
 newptr->n = n;
+
+if (idx == 0)
+{
+newptr->next = *head;
+*head = newptr;
+return (newptr);
+}
 if (head == NULL)
 newptr->next = NULL;
 return (NULL);
 ch = *head;
 for (i = 0; i < idx; i++)
 {
-ch = ch->next;
-}
+if (i == idx - 1)
+{
 newptr->next = ch->next;
 ch->next = newptr;
 return (newptr);
+}
+ch = ch->next;
+}
+free(newptr);
+return (NULL);
 }
