45a07f847df2561d5098c4bf1c7be41fca84c48f
Eztosin <tosinezekiel@gmail.com>
2023-07-10 14:26:10 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,6 +14,9 @@ int fd, wrttn_bytes, txt_len;
 if (filename == NULL)
 return (-1);
 
+if (text_content == NULL)
+text_content = "";
+
 if (text_content != NULL)
 {
 for (txt_len = 0; text_content[txt_len] != '\0'; txt_len++)
@@ -22,16 +25,14 @@ fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 if (fd == -1)
 return (-1);
+
 if (text_content != NULL)
-{
 wrttn_bytes = write(fd, text_content, txt_len);
+
 if (wrttn_bytes == -1)
-{
 close(fd);
 return (-1);
-}
-}
-else
+
 close(fd);
 return (1);
 }
